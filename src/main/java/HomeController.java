92dd54a288bfb1ced440527e4c2db9dd47a5fbca
tak <taks8238@gmail.com>
2019-03-31 19:49:31 +0900

    add HomeController.

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,10 @@
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping(value = "/")
+    public String home() {
+        return "index";
+    }
+}
